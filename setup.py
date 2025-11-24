dc2aec0e95ed6e6cac336b98b433a954bcd400fe
Bryce Willey <bryce.willey@suffolk.edu>
2025-11-24 16:58:57 -0500

    Use implicit namespaces over explicit namespaces

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -52,8 +52,7 @@ setup(name='docassemble.MAInformalReplyBriefForm',
       author_email='john.brandt@su.suffolk.edu',
       license='Suffolk LIT Lab',
       url='https://suffolklitlab.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=['docassemble.AssemblyLine>=3.2.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MAInformalReplyBriefForm/', package='docassemble.MAInformalReplyBriefForm'),
